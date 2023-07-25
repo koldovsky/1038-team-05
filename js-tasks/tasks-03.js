90429322e48f759d4bcfb127c74578c48f7bc65b
AlinaKhudokormova <135869045+AlinaKhudokormova@users.noreply.github.com>
2023-07-25 21:13:35 +0300

    Update tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -1,1 +1,8 @@
+Find the smallest integer in the array
+https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
 
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
+}
