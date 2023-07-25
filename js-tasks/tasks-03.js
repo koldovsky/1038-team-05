1741499f7fdbb556f108a1ff4fb6463a58795b15
AlinaKhudokormova <135869045+AlinaKhudokormova@users.noreply.github.com>
2023-07-25 21:43:33 +0300

    Update tasks-03.js

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -1,8 +1,13 @@
-Find the smallest integer in the array
-https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+//Find the smallest integer in the array https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
 
 class SmallestIntegerFinder {
   findSmallestInt(args) {
-    return Math.min(...args)
+    return Math.min(...args);
   }
+}
+
+//Geometry Basics: Circle Circumference in 2D https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+
+function circleCircumference(circle) {
+    return 2 * Math.PI * circle.radius;
 }
