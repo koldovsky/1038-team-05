91fb7eca27c347f71956395f5fb84fe37b77dcaa
Jane <131306802+JaneRainbow2@users.noreply.github.com>
2023-07-26 15:25:40 +0000

    add

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -24,7 +24,7 @@ function circleCircumference(circle) {
 
 function giveMeFive(obj) {
   const arr = [];
-  for (let key in obj) {
+  for (const key in obj) {
     if (key.length == 5) arr.push(key);
     if (obj[key].length == 5) arr.push(obj[key]);
   }
@@ -35,8 +35,8 @@ function giveMeFive(obj) {
 
 function buildFun(n) {
   const res = [];
-  for (let i = 0; i < n; i++) {
-    let tmp = i;
+  for (const i = 0; i < n; i++) {
+    const tmp = i;
     res.push(function () {
       return tmp;
     });
