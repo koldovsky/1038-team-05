232d85fbb0ea2905928c352acfa849a5bbede963
Stowpachenko <79781386+Stowpachenko@users.noreply.github.com>
2023-08-11 22:49:35 +0300

    Update header_modal_menu.js

--- js/header_modal_menu.js
+++ js/header_modal_menu.js
@@ -1,24 +1,22 @@
  export const modal = () => {
  
     const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
+      modalBtns: document.querySelectorAll('.list-logo__modal-btn'),
+      back: document.querySelector('.backdrop')
     };
 
-
-   
-  
-    console.log(refs.openModalBtn)
-    refs?.openModalBtn?.addEventListener('click', toggleModal);
-    refs?.closeModalBtn?.addEventListener('click', toggleModal);
+    for (var i = 0; i < refs?.modalBtns?.length; i++) {
+      refs?.modalBtns[i]?.addEventListener('click', toggleModal);
+    }
   
     function toggleModal() {
-      document.body?.classList.toggle('modal-open');
-      refs.modal?.classList.toggle('is-hidden');
+      document.body.classList.toggle('modal-open');
+      refs?.back?.classList.toggle('is-hidden');
+      this.classList.toggle('open');
+      document.querySelector('.' + this.getAttribute('data-modal')).style.display = 'block';
       if(document.body.classList.contains('modal-open')) {
         document.body.style.overflow = 'hidden';
-      } else{
+      } else {
         document.body.style.overflow = '';
       }
     }
